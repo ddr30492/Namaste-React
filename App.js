b554428f23bcd3646bb5c1a8952ddcd0f68adb40
Deep Darji <deepdarji@Deeps-MacBook-Air.local>
2025-05-02 01:15:00 +0530

    all code episode 01

---
+++ App.js
@@ -0,0 +1,48 @@
+const heading = React.createElement(
+  "h1",
+  {
+    className: "heading",
+    id: "heading",
+    style: {
+      color: "blue",
+      textAlign: "center",
+      fontSize: "50px",
+    },
+    onClick: () => {
+      alert("Hello, React!");
+    },
+  },
+  "First, React Program Using CDN!"
+);
+// Create an h1 element with text it will retrun an object
+console.log(heading);
+
+const parent = React.createElement(
+  "div",
+  {
+    className: "parent",
+    id: "parent",
+  },
+  [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am h1 tag"),
+      React.createElement("h2", {}, "I am h2 tag"),
+    ]),
+  ]
+); // Create a parent div element and add the heading as a child
+
+console.log(parent);
+
+// Create a root element and render the heading
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+
+
+// for creating econd root and rendering the heading
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+root2.render(heading);
---
+++ README.md
@@ -0,0 +1,7 @@
+# React learning 
+# Episode 01
+# What is react and why use it ?
+# difference btween library and framework
+# differecne between react vs reactDOM
+# what is cross origin
+# difference between async and defer in script tag attribute
